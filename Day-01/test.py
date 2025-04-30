996a0983a26c16e81aea1a3c9e86f499fc79ecba
Fernando Retagi <fernando.retagi@gmail.com>
2025-04-30 23:21:11 +0000

    Adding 1st file do day 1

---
+++ Day-01/test.py
@@ -0,0 +1,9 @@
+arn = "arn:aws:iam::123456789012:user/johndoe"
+print(arn.split("/")[1])
+
+str1 = "Hello"
+str2 = "World"
+print(f"{str1} {str2}")
+
+text = "Python is awesome"
+print("The lenght of text is: ", len(text))
